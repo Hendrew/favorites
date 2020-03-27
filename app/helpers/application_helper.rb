72b5df202c7a627e7d8f4bf8f8a4fabc27bf0de2
Hendrew Hélio <hendrew.developer@gmail.com>
2020-03-27 09:22:23 -0300

    devise setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def flash_message_class(level)
+    case level.to_s
+      when 'notice'
+        'is-success'
+      when 'success'
+        'is-success'
+      when 'info'
+        'is-info'
+      when 'error'
+        'is-danger'
+      when 'alert'
+        'is-danger'
+      when 'danger'
+        'is-danger'
+    end
+  end
 end
